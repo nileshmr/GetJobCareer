b39c630131840d8e553b46453c969b663220b3d8
Nilesh Maurya <134406657+nileshmr@users.noreply.github.com>
2026-09-04 17:51:12 +0530

    Add sitemap integration to Astro config

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://getjobcareer.com",
   output: "server",
   adapter: cloudflare(),
+  integrations: [sitemap()],
 });
